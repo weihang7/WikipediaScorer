647c41f1b321597fc17cacc31357ae6dd68ceb2a
dabbler0 <dab1998@gmail.com>
2012-10-06 17:03:54 -0400

    Changed README.md to actually say things.

--- Counter.java
+++ Counter.java
@@ -2,24 +2,43 @@ import java.util.*;
 
 class Counter {
 	static Hashtable[] count(String[] corpus) {
+		/*
+		 * Given an array of tokens (corpus),
+		 * constructs a degree 2 Markov model out of it
+		 * and returns both (occurs), the occurence probabilities
+		 * of each token, and (bigrams), the transition probabilities
+		 * of each pair of tokens.
+		 */
+		
+		//Declare all our hashtables:
 		Hashtable occurs = new Hashtable();
 		Hashtable bigrams = new Hashtable();
-		Hashtable[] both = {occurs,bigrams};
-		Hashtable lastHash = new Hashtable();
+		Hashtable[] both = {occurs,bigrams}; //This is the thing we'll return
+		Hashtable lastHash = new Hashtable(); //This is the Markov transition probabiliities from the previous token
+
 		for (int i = 0; i < corpus.length; i += 1) {
 			if (!occurs.containsKey(corpus[i])) {
-				occurs.put(corpus[i], 1);
+				//If we've never seen this token before, set it up:
+				occurs.put(corpus[i], 1.0);
 				bigrams.put(corpus[i], new Hashtable());
 			}
+			else {
+				//Otherwise, add one to the number of times we've seen it
+				occurs.put(corpus[i], (Double)occurs.get(corpus[i]) + 1);
+			}
+			
 			if (!lastHash.containsKey(corpus[i])) {
-				lastHash.put(corpus[i], 1);
+				//If we've never seen this transition before, set it up:
+				lastHash.put(corpus[i], 1.0);
 			}
 			else {
-				lastHash.put(corpus[i], (Integer)lastHash.get(corpus[i]) + 1);
+				//Otherwise, add one to the number of times we've seen it.
+				lastHash.put(corpus[i], (Double)lastHash.get(corpus[i]) + 1.0);
 			}
+
+			//Update lastHash for the next iteration.
 			lastHash = (Hashtable)bigrams.get(corpus[i]);
 		}
-		lastHash.put("<EOF>",1);
 		return both;
 	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 WikipediaScorer
 ===============
+
+This application scores encyclopeadic writing based on statistical similarities to Wikipedia. It is written in Javam, for a computer science project at Phillips Exeter Academy.
