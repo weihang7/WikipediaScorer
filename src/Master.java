485ee039dbd0e2f754c0921ae6f17b7fb8c0435a
Weihang Fan <weihang_6@163.com>
2012-10-07 23:04:29 -0400

    RESTORE

---
+++ src/Master.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author weihangfan
+ *
+ */
+public class Master {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] pages = Fetcher.getRandomPageTexts(30);
+		String[][] words;
+		for (int i=0;i<pages.length;i++){
+			words = Tokenizer.fullTokenization(pages[i], 3000);
+		}
+	}
+}
