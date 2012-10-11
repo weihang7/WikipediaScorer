53189b65118d08f4d2d607a3a8ad8106b474d712
Weihang Fan <weihang_6@163.com>
2012-10-11 09:28:41 -0400

    add library poi to read from word document

---
+++ src/ReadFromFile.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.poifs.filesystem.*;
+
+import org.apache.poi.hwpf.*;
+
+import org.apache.poi.hwpf.extractor.*;
+
+public class ReadFromFile {
+
+	public static String readFromDoc(File docfile){
+		
+	}
+
+}
