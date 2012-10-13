3b28797ea16b1e9e00ee493da1ab2d6f4caf8aa9
Weihang Fan <weihang_6@163.com>
2012-10-12 20:31:01 -0400

    deleted ArticleProp, improved Master

--- src/Master.java
+++ src/Master.java
@@ -1,12 +1,21 @@
+import java.util.*;
 class Master {
 	public static void main(String[] args) {
 		// get parsed xml into pages
 		String[] pages = Fetcher.getRandomPageTexts(30);
 		//declare variable words
-		String[][] words;
+		ArrayList words = new ArrayList(pages.length);
 		//Split the pages into tokens.
 		for (int i=0;i<pages.length;i++){
-			words = Tokenizer.fullTokenization(pages[i], 3000);
+			words.add(Tokenizer.fullTokenization(pages[i], 3000));
 		}
+		//concatenate the pages
+		ArrayList ret = new ArrayList(words.size());
+		for (int i=0;i<words.size();i++){
+			for (int k=0;i<words.size();i++){
+				ret.add(words.get(i)[k]);
+			}
+		}
+	
 	}
 }
