58cc794fafcbce9dbf2b3719273f75239bede6d9
Weihang Fan <weihang_6@163.com>
2012-10-12 22:05:30 -0400

    completed a big chunk of Master

--- src/Master.java
+++ src/Master.java
@@ -10,12 +10,36 @@ class Master {
 			words.add(Tokenizer.fullTokenization(pages[i], 3000));
 		}
 		//concatenate the pages
-		ArrayList ret = new ArrayList(words.size());
-		for (int i=0;i<words.size();i++){
-			for (int k=0;i<words.size();i++){
-				ret.add(words.get(i)[k]);
+		ArrayList firstHalf = new ArrayList(words.size()/2);
+		for (int i=0;i<words.size()/2;i++){
+			for (int k=0;k<((ArrayList) words.get(i)).size();k++){
+				firstHalf.add(((String[])words.get(i))[k]);
 			}
 		}
-	
+		ArrayList secondHalf = new ArrayList(words.size()/2);
+		for (int i=words.size()/2;i<words.size();i++){
+			for (int k=0;k<((ArrayList) words.get(i)).size();k++){
+				secondHalf.add(((String[])words.get(i))[k]);
+			}
+		}
+		Hashtable[] firstCountResult=Counter.count((String[])firstHalf.toArray());
+		Hashtable[] secondCountResult=Counter.count((String[])secondHalf.toArray());
+		Hashtable smootherResult = new Hashtable();
+		String[] firstKeys=(String[]) firstCountResult[1].keySet().toArray();
+		Hashtable occurence = Smoother.heldOutSmoothingScaledLogarithmic(
+				firstCountResult[0],
+				secondCountResult[0],
+				3000);
+		Hashtable transition = new Hashtable();
+		for (int i = 0;i<firstKeys.length;i++){
+			transition.put(
+					firstKeys[i],
+					Smoother.heldOutSmoothingScaledLogarithmic(
+							(Hashtable)firstCountResult[1].get(firstKeys[i]),
+							(Hashtable)secondCountResult[1].get(firstKeys[i]), 
+							3000
+					)
+			);
+		}
 	}
 }
