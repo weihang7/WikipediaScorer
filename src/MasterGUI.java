7e10212110293d221023389df09447c044556dc9
Raghav <mr.ragbag@gmail.com>
2012-11-01 11:19:27 -0400

    Started MasterGUI

---
+++ src/MasterGUI.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+public class MasterGUI extends JFrame {
+	 public static JFrame createGUI(){
+		 JFrame f = new JFrame("MasterGUI");
+		 return f;
+	 }
+	 }
+	
+
+
