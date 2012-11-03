4011cb17888ba294413749cf447d3bd1eea0e130
Raghav <mr.ragbag@gmail.com>
2012-11-03 11:20:51 -0400

    AlphaCount Final

--- src/AlphabetCount.java
+++ src/AlphabetCount.java
@@ -2,15 +2,21 @@ import java.util.HashMap;
 
 public class AlphabetCount {
 	HashMap<String, Integer> alphaCount;
-public AlphabetCount(){ 
-	alphaCount =  new HashMap<String, Integer>();
-}
-private int storeInHashMap(){
-	if()
-	return 12;
-}
-public void hashComponent(){
+	public AlphabetCount(){ 
+		alphaCount =  new HashMap<String, Integer>();
+	}
 	
-}
-
+	public int get(String x){
+		return alphaCount.get(x);
+		
+	}
+	public void add(String x, int n){
+	
+		if (alphaCount.containsKey(x)){
+			alphaCount.put(x,alphaCount.get(x)+n);
+		}
+		else {
+			alphaCount.put(x, n);
+		}
+	}
 }
