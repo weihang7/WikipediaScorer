1ffd3887f263f8e014bad92ab7964b8b570c4b7e
Raghav <mr.ragbag@gmail.com>
2012-11-03 11:37:23 -0400

    MasterGui

--- src/MasterGUI.java
+++ src/MasterGUI.java
@@ -8,12 +8,17 @@ public class MasterGUI extends JFrame {
 	 }
 	 public JPanel makeTopPanel(){
 		 JPanel topPanel= new JPanel();
+		 JLabel pages = new JLabel("Number of Pages to Fetch");
+		 topPanel.add(pages);
+		 return makeTopPanel;
 	 }
 	 public JPanel makeMiddlePanel(){
 		 JPanel middlePanel = new JPanel();
+		 return makeMiddlePanel;
 	 }
 	 public JPanel makeBottomPanel(){
 		 JPanel bottomPanel = new JPanel();
+		 return makeBottomPanel;
 	 }
 	 public static void main(String [] args){
 		 MasterGUI gui=new MasterGUI();
