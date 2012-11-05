9bd2d68d9780c3a960fd1de5928cd310bb15a62c
MacUser <macuser@ITS000514M.local>
2012-11-05 09:41:40 -0500

    Anthony: Debugged Master

--- src/Counter.java
+++ src/Counter.java
@@ -5,13 +5,12 @@ import java.util.Enumeration;
  */
 
 class Counter {
-  public double[][] count(Enumeration<Integer> document, int alphabetSize) {
+  public static double[][] count(Enumeration<Integer> document, int alphabetSize) {
     double[][] r = new double[alphabetSize + 1][alphabetSize];
     
     for (int last = document.nextElement(); document.hasMoreElements();) {
       r[last + 1][(last = document.nextElement())] += 1;
     }
-
     return r;
   }
 }
