2c33774d8951d4724701df202018651debbf7175
dabbler0 <dab1998@gmail.com>
2012-11-10 14:02:51 -0500

    Rewrote most of DataSet. It's the same in functionality but I think it's a bit neater and easier to work off of.

--- src/DataSet.java
+++ src/DataSet.java
@@ -1,134 +1,222 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.StringReader;
-import java.sql.Connection;
+import org.sqlite.JDBC;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Enumeration;
+import java.util.HashMap;
 
-import javax.swing.JOptionPane;
+class DataSet {
+  private File db;
+  private String dbPath;
+  private File[] wikis;
+  private File[] tokens;
 
-import org.apache.poi.hwpf.HWPFDocument;
-import org.apache.poi.hwpf.extractor.WordExtractor;
-import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
+  public class BufferedDatabaseWriter {
+    private static BUFFER_LIMIT = 800;
+    private File db;
+    private String dpath = db.getAbsolutePath();
+    private HashMap<String, Integer> buffer;
 
-/**
- * @author Weihang Fan
- *
- */
-public class DataSet {
-  // Field declarations
-  private File database;
-  private File goodWiki;
-  private File badWiki;
-  private File goodTokens;
-  private File badTokens;
-
-  // Define the constructor, which takes the path of the file in String.
-  public DataSet(String baseFilePath){
-    database = new File(baseFilePath);
-  }
-  
-  // Define the function that executes the commands to the database field.
-  public void execute(String[] commands) throws ClassNotFoundException{
-    
-    Class.forName("org.sqlite.JDBC");
-    Connection connection = null;
-    
-    try{
-      connection = DriverManager.getConnection("jdbc:sqlite:"+database.getAbsolutePath());
-      Statement statement = connection.createStatement();
-      for (int i=0;i<commands.length;i++)
-        statement.executeUpdate(commands[i]);
+    public BufferedDatabaseWriter(File db) {
+      this.db = db;
     }
-    catch(SQLException e){
-      e.printStackTrace();
-    }
-    finally{
-      try{
-        if(connection!=null){
-          connection.close();
-        }
+
+    public void flush() {
+      //Set up a command to select all pertinent rows:
+      String command = "SELECT * FROM alphabet WHERE token IN (";
+
+      for (Enumeration<String> keys = buffer.keys(); keys.hasMoreElements();) {
+        command += keys.nextElement() + (keys.hasMoreElements() ? ", " : ")");
       }
-      catch(SQLException e){
-      System.err.println(e);
+      
+      //Open a connection
+      Connection connection = DriverManager.getConnection("jdbc:sqlite:" + dpath);
+      //Make a query to that connection for pertinent rows:
+      Statement statment = connection.createStatement;
+      ResultSet results = statement.executeQuery(command);
+      
+      //Find which columns contain the counts and the token names:
+      int countColumn = results.findColumn("count");
+      int nameColumn = results.findColumn("token");
+
+      //Add our counts to the existent counts and empty the hashtable as we move:
+      while (results.next()) {
+        String name = results.getString(nameColumn);
+        results.updateInt(countColumnm,
+                          results.getInt(countColumn) + buffer.get(name));
+        buffer.remove(name);
+      }
+
+      //Any values that were not emptied (are not already in the table), we should add:
+      for (Enumeration<String> keys = buffer.keys(); keys.hasMoreElements();) {
+        key = keys.nextElement();
+        statment.executeUpdate("INSERT INTO alpahbet VALUES (" + key + "," + buffer.get(key));
+        buffer.remove(key);
       }
     }
+
+    public add(String key, int value) {
+      buffer.put(key, (buffer.contains(key) ? buffer.get(key) : 0) + value);
+      if (buffer.size() > BUFFER_LIMIT) flush();
+    }
   }
+
+  public DataSet (String baseFilePath) {
+    //Initialize all the files we link to.
+    db = new File(baseFilePath + "_db");
+    dpath = db.getAbsolutePath();
+    wikis[0] = new File(baseFilePath + "_w_g");
+    wikis[1] = new File(baseFilePath + "_w_b");
+    tokens[0] = new File(baseFilePath + "_t_g");
+    tokens[1] = new File(baseFilePath + "_t_b");
+  }
+
+  private void execute(String[] sql) throws SQLException {
+    //General method for executing non-SELECT sql statements.
+
+    //Open a connection to our database.
+    Connection connection = DriverManager.getConnection("jdbc:sqlite:"+dpath);
+
+    //Get ready to make a statement.
+    Statement statement = connection.createStatement();
+    
+    //Execute the commands.
+    for (int i = 0; i < sql.length; i += 1) {
+      statement.executeUpdate(sql);
+    }
+    
+    //Close the connection.
+    connection.close();
+  }
+
+  //---------------------COUNT TABLES--------------------------
   
-  public void createTable(String name,double[][] cells){
-    String[] commandsToExecute = new String[2];
-    commandsToExecute[0] = "DROP TABLE if exists "+name;
-    commandsToExecute[1] = "CREATE TABLE "+name+" ("; 
-    for (int i=0;i<cells[0].length;i++){
-      commandsToExecute[1]+="\""+i+"\""+ " DOUBLE"+(i==cells[0].length-1?"":",");
+  private void createCountTable(String name, int alphabetSize) throws SQLException {
+    
+    //The commands we need to make a table:
+    String[] commands = {
+      "DROP TABLE IF EXISTS " + name,
+      "CREATE TABLE " + name + "(name DOUBLE, "
+    };
+    
+    for (int i = 0; i < alphabetSize; i += 1) {
+      //Add the necessary columns to our table.
+      commands[1] += "\"" + i + "\" DOUBLE"
+                  + (i == cells[0].length - 1 ? ")" : ",");
     }
-    commandsToExecute[1]+=")";
-    try {
-      execute(commandsToExecute);
-    } 
-    catch (Exception e) {
-      e.printStackTrace();
-    }
-    for(int i=0;i<cells.length;i++){
-    insert("test",cells[i]);
-    }
+
+    //Execute the commands.
+    execute(commands);
   }
+
+  private void insertCount(String table, int index, double[] value) {
+    //The command that we need to insert a row:
+    String[] commands = {"INSERT INTO " + table
+                         + " VALUES (" + index + ", "};
+
+    //Add the necessary values to our row:
+    for (int i = 0; i < value.length; i = 1) {
+      commands[0] += value[i] + (i == value.length - 1 ? ")" : ", ");
+    }
+    
+    //Execute the command.
+    execute(commands);
+  }
+
+  private void updateCount(String table, int index, double[] value) {
+    /*
+      Probably won't actually use this. If possible, use the addCount() method.
+    */
+
+    //The command that we need to update a row:
+    String[] commands = {"UPDATE " + table
+                      +  " SET "};
+
+    //Add the necessary values:
+    for (int i = 0; i < value.length; i += 1) {
+      commands[0] += i + "=" + value[i] + (i == value.length ? "" : ", ");
+    }
   
-  public void insert(String table,double[] element){
-    String[] commandsToExecute = new String[1];
-    commandsToExecute[0]="insert into "+table+" (";
-    for(int i=0;i<element.length;i++){
-      commandsToExecute[0]+="\""+i+"\""+(i==element.length-1?"":",");
-    }
-    commandsToExecute[0]+=") values (";
-    for(int i=0;i<element.length;i++){
-      commandsToExecute[0]+=element[i]+(i==element.length-1?"":",");
-    }
-    commandsToExecute[0]+=")";
-    System.out.println(commandsToExecute[0]);
-    try{
-      execute(commandsToExecute);
-    }
-    catch(Exception e){
-      e.printStackTrace();
-    }
+    //Add the WHERE statement.
+    commands[0] += "WHERE name=" + index;
+    
+    //Execute the commands.
+    execute(commands);
   }
-  
-  public double[][] loadAll() throws Exception{
-  Class.forName("org.sqlite.JDBC");
-  Connection connection = null;
-  connection = DriverManager.getConnection("jdbc:sqlite:"+database.getAbsolutePath());
-  Statement statement = connection.createStatement();
-  ResultSet wholeTable = statement.executeQuery("select * from test");  
-  ResultSetMetaData rsmd = wholeTable.getMetaData();
-  int numberOfColumns = rsmd.getColumnCount();
-  ArrayList<double[]> ret = new ArrayList<double[]>();
-  double[] currentArray = new double[numberOfColumns];
-  while(wholeTable.next()){
-    currentArray = new double[numberOfColumns];
-    for(int i=1;i<=numberOfColumns;i++){
-    currentArray[i-1] = wholeTable.getDouble(i);
+
+  private double[][] selectCounts(String table, int[] indices) {
+    String commands = "SELECT * FROM " + table
+                    + " WHERE name IN (";
+
+    //Specify which rows we're selecting.
+    for (int i = 0; i < indices.length; i += 1) {
+      commands += indices[i] + (i == indices.length ? ")" : ", ");
     }
-    ret.add(currentArray);
+    
+    //Open our connection.
+    Connection connection = DriverManager.getConnection("jdbc:sqlite:" + dpath);
+
+    //Create our statement.
+    Statement statement = connection.createStatement();
+
+    //Make our query.
+    ResultSet result = statement.executeQuery(command);
+
+    //Scroll to the first result.
+
+    //See how many columns there are:
+    int len = result.getMetaData.getColumnCount();
+    double[][] r = new double[indices][len - 1];
+    
+    //Scroll through our results and put them into a big array of doubles.
+    for (int i = 1; i <= indices.length; i += 1) {
+     result.absolute(i);
+     for (int x = 1; x < len; x += 1) {
+       r[i][x] = result.getDouble(i);
+     }
+    }
+
+    return r;
   }
-  connection.close();
-  wholeTable.close();
-  return ret.toArray(new double[ret.size()][numberOfColumns]);
+
+
+  //---------------------ALPHABET COUNT TABLES---------------------
+  private void createAlphabetCountTable(String name) {
+    //Execute this command.
+    String[] commands = {
+      "DROP TABLE IF EXISTS " + name,
+      "CREATE TABLE " + name + "(name STRING, count INTEGER)"
+    };
+    execute(commands);
   }
-  
+
+  public BufferedDatabaseWriter writeAlphabet() {
+    //Instatiate a new BufferedDatabaseWriter for our alphabet.
+    return new BufferedDatabaseWriter(db);
+  }
+
+  public String[] getBestAlphabet(int alphabetSize) {
+    //Get the best (alphabetSize) results (ordered by token count) from our database.
+    String[] r = new String[alphabetSize];
+
+    Connection connection = DriverHandler.getConnection("jdbc:sqlite:"+dpath);
+    Statement statement = connection.createStatment();
+    ResultSet results = statement.executeQuery(
+      "SELECT * FROM alphabet ORDER BY count ASC"
+    );
+
+    int nameColumn = results.findColumn("token");
+    
+    for (int i = 0; i < alphabetSize; i += 1) {
+      results.next();
+      r[i] = results.getString(nameColumn);
+    }
+
+    return r;
+  }
+
+  //--------------------NORMAL FILE IO--------------------------
+  /*
+   * This should just be copied over from the old DataSet. It was fine.
+  */
   private class WordReader implements Enumeration<String> {
     StringReader r;
     public WordReader(File f) {
@@ -179,105 +267,4 @@ public class DataSet {
   
   public Enumeration<Integer> readTokens(boolean which) {
     return new TokenReader((which ? goodTokens : badTokens));
-  }
-  
-  public void finalizeAlphabet(String[] alphabet){
-    for(int i=0;i<alphabet.length;i++){
-      append("alphabet",alphabet[i]);
-    }
-  }
-  
-  public static void append(String file, String value){
-    try{
-      // Create file 
-      FileWriter fstream = new FileWriter(file,true);
-      BufferedWriter out = new BufferedWriter(fstream);
-      out.write(value);
-      //Close the output stream
-      out.close();
-    }
-    catch (Exception e){
-      //Catch exception if any
-      System.err.println("Error: " + e.getMessage());
-    }
-  }
-  
-  public static void overwrite(String stringToWrite, String path){
-      try{
-        FileWriter fr = new FileWriter(path);
-        BufferedWriter br = new BufferedWriter(fr);
-        br.write(stringToWrite);
-        br.close();
-      }
-      catch(Exception ex){
-        ex.printStackTrace();
-      }
-  }
-  
-  public static BufferedInputStream loadStringStream(String file) throws IOException{
-    // Create file
-    FileInputStream is = new FileInputStream(new File(file));
-    return new BufferedInputStream(is);
-  }
-  
-  public static String readFromDoc(File docfile){
-    
-    //Establishing the things to receive data from the FileInputStream
-    String ret = "";
-    
-    try {
-      //Create a FileInputStream that links the HWPFDocument and a WordExtractor
-      FileInputStream fis=new FileInputStream(docfile);
-      HWPFDocument document=new HWPFDocument(fis);
-      WordExtractor extractor = new WordExtractor(document);
-      
-      //Extract the text
-      ret=extractor.getText();
-    }
-    catch(Exception exep){
-      // Catch exceptions
-      JOptionPane.showMessageDialog(null,"docLoadingException");
-    }
-    
-    return ret;
-  }
-  
-  public static String readFromDocx(File docxfile){
-
-    //Establishing the things to receive data from the FileInputStream
-    String ret = "";
-    
-    try {
-      //Create a FileInputStream that links the HWPFDocument and a WordExtractor
-      FileInputStream fis=new FileInputStream(docxfile);
-      XWPFDocument document=new XWPFDocument(fis);
-      XWPFWordExtractor extractor = new XWPFWordExtractor(document);
-      
-      //Extract the text
-      ret=extractor.getText();
-    }
-    catch(Exception exep){
-      // Catch exceptions
-      JOptionPane.showMessageDialog(null,"docxLoadingException");
-    }
-    
-    return ret;
-  }
-  
-  //Reading from a text file
-  public static String readFromText(File txtfile){
-    String text="";
-    try {
-      FileReader fr=new FileReader(txtfile);
-      BufferedReader br=new BufferedReader(fr);
-      String currentline = "";
-      while((currentline=br.readLine())!=null) {
-        text+=currentline;
-      }
-      br.close();
-     } catch(Exception ex) {
-       ex.printStackTrace();
-     }
-     return text;
-  }
 }
--- src/Master.java
+++ src/Master.java
@@ -57,27 +57,27 @@ class Master {
       Tokenizer.countForAlphabet(alphabetWriter, data.readWiki(true));
       Tokenizer.countForAlpahbet(alphabetWriter, data.readWiki(false));
 	
-	  //Get best alphabet:
+      //Get best alphabet:
       String[] alphabet = data.getBestAlphabet(ALPHABET_SIZE);
 	  
-	  //Store it somewhere:
+      //Store it somewhere:
       data.finalizeAlphabet(alphabet);
 	
-	  //Strip tokens:
+      //Strip tokens:
       Tokenizer.stripTokens(data.readWiki(true), data.writeTokens(true), alphabet);
       Tokenizer.stripTokens(data.readWiki(false), data.writeTokens(false), alphabet);
 	
-	  //COUNTING:
+      //COUNTING:
       Enumeration<Integer> readGoodTokens = data.readTokens(true);
       Enumeration<Integer> readBadTokens = data.readTokens(false);
       double[][][] goodCounts = {Counter.count(readGoodTokens, (goodLength + 1) / 2), Counter.count(readGoodTokens, goodLength / 2)};
       double[][][] badCounts = {Counter.count(readBadTokens, (badLength + 1) / 2), Counter.count(readBadTokens, badLength / 2)};
 	
-	  //SMOOTHING:
+      //SMOOTHING:
       double[][] smoothGoodCounts = Smoother.smooth(goodCounts[0], goodCounts[1]);
       double[][] smoothgBadCounts = Smoother.smooth(badCounts[0], badCounts[1]);
 	
-	  //Store smoothed counts:
+      //Store smoothed counts:
       data.add(true, smoothGoodCounts);
       data.add(false, smoothBadCounts);
   }
